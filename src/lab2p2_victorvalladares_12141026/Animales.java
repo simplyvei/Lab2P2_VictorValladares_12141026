075d848b67a2bf68b63d1a1b1b0950e1e230f571
simplyvei <98425827+simplyvei@users.noreply.github.com>
2022-01-28 13:34:11 -0600

    Se agregaron atributos y constructor

--- src/lab2p2_victorvalladares_12141026/Animales.java
+++ src/lab2p2_victorvalladares_12141026/Animales.java
@@ -10,5 +10,24 @@ package lab2p2_victorvalladares_12141026;
  * @author valla
  */
 public class Animales {
+    private String nombreC;
+    private String nombreComun;
+    private String habitat;
+    private String alimentacion;
+    private String descripcion;
+    private String geografica;
+    private int vida;
     
+    public Animales(){
+        
+    }
+    public Animales(String nombreC, String nombreComun, String habitat, String alimentacion, String descripcion, String geografica, int vida){
+        this.nombreC = nombreC;
+        this.nombreComun = nombreComun;
+        this.habitat = habitat;
+        this.alimentacion = alimentacion;
+        this.descripcion = descripcion;
+        this.geografica = geografica;
+        this.vida = vida;
+    }
 }
